5508d514a796a4a8879b3a71e4e79a569cf09807
Wavyspla9980 <tr0lich@yandex.ru>
2025-02-25 17:49:32 +0300

    ore

--- src/main/java/com/example/Thewavluy.java
+++ src/main/java/com/example/Thewavluy.java
@@ -5,16 +5,16 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.example.item.ore.MixinIronDiamonds;
+
+
 public class Thewavluy implements ModInitializer {
 	public static final String MOD_ID = "the-wav-luy";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		
+		MixinIronDiamonds.reg(MOD_ID);
 	}
 }
--- src/main/java/com/example/item/ore/MixinIronDiamonds.java
+++ src/main/java/com/example/item/ore/MixinIronDiamonds.java
@@ -1,5 +1,26 @@
-package com.example.mixin.item.ore;
+package com.example.item.ore;
+
+import com.example.Thewavluy;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 public class MixinIronDiamonds {
+    public static final Item ore = Registry.register(Registries.ITEM, 
+        
+    new Identifier(Thewavluy.MOD_ID,"mixin"), 
+    new Item(new FabricItemSettings().food(null)));
 
+    public static final Item reg(String name){
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(e -> {
+            e.add(ore);
+        });
+
+        return ore;
+    }
 }
